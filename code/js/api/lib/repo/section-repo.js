a7075231c2aae2f78078a1105baed3ca0da243d6
nrcardeal <n.rafaelcardeal@hotmail.com>
2021-06-25 18:05:48 +0100

    Added some files missing in last commit

---
+++ code/js/api/lib/repo/section-repo.js
@@ -0,0 +1,51 @@
+'use strict'
+
+
+const db = require('./queuality-db.js').methods
+const Error = require('../common/error.js')
+const { Section } = require('../common/model.js') // eslint-disable-line no-unused-vars
+
+const collection = 'section'
+
+/**
+ * Gets an array of all the sections in the database.
+ * @returns {Promise<Array<Section>>} Array of Section
+ */
+const getSections = () => db.getAll(collection)
+
+/**
+ * Gets a section from the database by its name.
+ * @param {String} name The name of the section
+ * @returns {Promise<Section>} Section
+ */
+const getSection = (name) => db.getByProperties(collection, {name: name})
+    .then((section) => {
+        if (!section)
+            throw new Error.CustomException(`The section ${section} is not in the database`, Error.NOT_FOUND)
+        return section
+    })
+
+/**
+ * Inserts a new section into the database
+ * @param {Section} section Section to be inserted
+ * @returns {Promise<Section>}
+ */
+const insertSection = (section) => db.insert(collection, section)
+
+/**
+ * Updates a section in the database with the same name that is passed in the parameter 
+ * @param {Section} section Section to replace the the one with the same name
+ * @returns {Promise<Section>}
+ */
+const updateSection = (section) => getSection(section.name)
+    .then(() => db.update(collection, section.name, section))
+
+/**
+ * Deletes a section given its name 
+ * @param {String} name Name of the section to be deleted
+ * @returns {Promise<Section>}
+ */
+const deleteSection = (name) => getSection(name)
+    .then(() => db.del(collection, name))
+
+module.exports = {getSections, getSection, insertSection, updateSection, deleteSection}
